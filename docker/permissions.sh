2fb3cad0c19d6a4ae93eba54b8a8a2370651fddf
Валентин Немцев <mail@valentin-nemcev.com>
2017-11-01 10:23:22 +0300

    Fix permissions

--- docker/permissions.sh
+++ docker/permissions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ "$DOCKER_UID" && "$DOCKER_UID" != "0" ]]; then
   groupadd --non-unique --gid $DOCKER_GID appgroup;
-  useradd -K MAIL_DIR=/dev/null --uid $DOCKER_GID --groups appgroup --home-dir /root --no-create-home appuser;
+  useradd -K MAIL_DIR=/dev/null --uid $DOCKER_UID --groups appgroup --home-dir /root --no-create-home appuser;
 fi;
 
 exec sudo -u appuser "$@";
